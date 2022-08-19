9fa24839d9f0793fa856da7ad7430afc57abeac5
alyesc <guopengjun1998@163.com>
2022-08-19 22:31:13 +0800

    construct

--- string.cpp
+++ string.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
+#include <string.h>
 
-class string {
+class mystring {
 public:
-    string();
-    string(const char *str);
-    string(const string &rhs);
-    string& operator=(const string&rhs);
-    ~string();
+    mystring();
+    mystring(const char *str);
+    mystring(const mystring &rhs);
+    mystring& operator=(const mystring&rhs);
+    ~mystring();
 
     char operator[](int n) const;
     int size() const;
     char *c_str() const;
 
 private:
-    char *m_str;
+    char *m_pData;
 };
+
+mystring::mystring():m_pData(new char[1]) {
+    *m_pData = '\0';
+}
+
+mystring::mystring(const char *str){
+    if(!str) {
+        m_pData = new char[1];
+        *m_pData = '\0';
+    } else {
+        int nLen = strlen(str);
+        m_pData = new char[nLen + 1];
+        strcpy(m_pData, str); 
+    }
+}
+
+mystring::mystring(const mystring &rhs) {
+    
+}
